18f9639bf19e42832692fbb0030e1469329fd504
HarshitaAgarwal09 <harshitaagarwal98@yahoo.com>
2019-01-23 23:08:30 +0530

    Todo Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaskService } from './task.service';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TODO';
-  tasks;
+   task = {
+    name :"",
+    id :0,
+    strike:false
+  };
+  tasks = [];
   constructor(private ts:TaskService){
-
   }
+
+  ngOnInit(){
+    this.getTasks();
+  }
+
+  onClick(){
+    this.ts.addTask(this.task);
+    this.task={
+      name:"",
+      id:0,
+      strike:false
+    };
+  }
+
+  editTask(task){
+    this.task = task;
+  }
+
   getTasks(){
-  	this.ts.gettasks()
+  	this.tasks = this.ts.getTasks();
+  }
+
+  deleteTask(task){
+  this.ts.deleteTask(task);   
+  }
+  strikeTask(task){
+  this.tasks = this.ts.strikeTask(task);
   }
 }
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -4,7 +4,54 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TaskService {
+	tasks= [
+	] ;
 
   constructor() { }
-  getTasks(){}
+
+  getTasks(){
+  	return this.tasks;
+  }
+
+  addTask(task){
+  	if(task.id == 0) {
+  		this.tasks.push({id:(new Date()).getTime(),name:task.name,strike:false});
+  	}
+  	else {
+  		this.editTask(task);
+  	}
+  }
+
+  editTask(task){
+  	for( var i = 0; i< this.tasks.length ; i++){
+	  		if(task.id == this.tasks[i].id){
+	  			this.tasks[i] = task;
+	  			break;
+	  		}
+	  	}
+  }
+
+  deleteTask(task){
+  	for( var i = 0; i< this.tasks.length ; i++){
+	  	if(task.id == this.tasks[i].id){
+	  		this.tasks.splice(i, 1);
+	  		break;
+	  	}
+	}
+  }
+
+  strikeTask(task){
+  	for( var i = 0; i< this.tasks.length ; i++){
+	  	if(task.id == this.tasks[i].id){
+	  		if(this.tasks[i].strike){
+	 				this.tasks[i].strike=false;
+	 			}
+	 			else{
+	 				this.tasks[i].strike=true;
+	 			}
+	 			break;
+		}
+	}
+	return this.tasks;
+  }
 }
